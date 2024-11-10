01238a238eda0ee6f177357b2ce0e53c6dbcf894
Gourav Raut <gouravraut@Gouravs-MacBook-Air.local>
2024-11-10 16:31:59 +0530

    passingData

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:bmi/IntorPage.dart';
+import 'package:bmi/newPage.dart';
 import 'package:bmi/splash_screen.dart';
 import 'package:flutter/material.dart';
 
@@ -27,77 +28,46 @@ class MyHomePage extends StatefulWidget {
   MyHomePage({super.key, required this.title});
   final String title;
 
-  final List<String> arrName = [
-    'hi',
-    'hello',
-    'apple',
-    'hi',
-    'hello',
-    'apple',
-    'hi',
-    'hello',
-    'apple'
-  ];
-  final List<int> arrNumber = [
-    12,
-    21,
-    34,
-    12,
-    21,
-    34,
-    12,
-    21,
-    34
-  ]; // Changed to a List
-  final List<int> arrchu = [
-    413212,
-    746521,
-    079834,
-    413212,
-    746521,
-    079834,
-    413212,
-    746521,
-    079834
-  ]; // Changed to a List
-
+  final List<String> arrName = ['hi', 'hello', 'apple', 'hi'];
+  final List<int> arrNumber = [12, 21, 34, 12];
+  final List<int> arrchu = [413212, 746521, 79834, 413212];
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+
+    var nameCon=TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: ListView.separated(
-        itemCount: widget.arrName.length, // Set the item count based on arrName
-        itemBuilder: (context, index) {
-          return ListTile(
-            leading: Text(
-              '${index + 1}',
-              style: TextStyle(fontSize: 25),
-            ),
-            title: Text(
-              widget.arrName[index],
-              style: TextStyle(fontSize: 30),
-            ),
-            subtitle: Text(
-              'Number: ${widget.arrNumber[index]}',
-              style: TextStyle(fontSize: 15),
-            ),
-            trailing: const Icon(
-              Icons.delete,
-            ),
-          );
-        },
-        separatorBuilder: (BuildContext context, int index) {
-          return const Divider(
-            height: 3,
-          );
-        },
+      body: Center(
+        child: Container(
+          width: 300,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              
+                TextField(
+                  controller: nameCon,
+                ),
+          
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => Newpage(nameCon.text.toString())),
+                  );
+                },
+                child: Text('Next'),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
